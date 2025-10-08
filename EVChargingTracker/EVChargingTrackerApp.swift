690749aa256abb7f4a5ba7e6dbbde6fbe387e96f
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-10-08 11:33:11 +0500

    Initial Commit

---
+++ EVChargingTracker/EVChargingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVChargingTrackerApp.swift
+//  EVChargingTracker
+//
+//  Created by Maxim Gorbatyuk on 08.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EVChargingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EVChargingTrackerTests/EVChargingTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EVChargingTrackerTests.swift
+//  EVChargingTrackerTests
+//
+//  Created by Maxim Gorbatyuk on 08.10.2025.
+//
+
+import Testing
+@testable import EVChargingTracker
+
+struct EVChargingTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
